4899808e43df0e2c9d76a5cfc573d9e5c786aa82
jiwonY <gkdnf87@gmail.com>
2024-09-24 23:59:14 +0900

    세부 설정

---
+++ src/main/java/com/example/bookhubbackend/BookhubBackendApplication.java
@@ -0,0 +1,12 @@
+package com.example.bookhubbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookhubBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BookhubBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bookhubbackend/BookhubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookhubbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookhubBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
