e2ae5d75505107fe70d8a3c1f9abba495d4a4616
jiwonY <gkdnf87@gmail.com>
2024-09-24 23:55:17 +0900

    사용자 인증 관련 예외 처리

---
+++ src/main/java/com/example/bookhubbackend/exception/UserAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.example.bookhubbackend.exception;
+
+public class UserAlreadyExistsException extends RuntimeException {
+    public UserAlreadyExistsException(String message) {
+        super(message);
+    }
+}
